c58488e69a08205af418f6d7a7e3842018ba7fea
haphop228 <krivoshein.2014@list.ru>
2025-07-19 02:00:59 +0300

    added grafana, prometheus and docker-compose

--- perl_pastebin/pastebin.pl
+++ perl_pastebin/pastebin.pl
@@ -2,13 +2,14 @@
 
 use utf8;
 use Mojolicious::Lite;
+use Mojolicious::Plugin::Prometheus;
+
 use DB_File;
 use Fcntl qw(:flock O_RDWR O_CREAT);
 use Digest::SHA qw(sha1_hex);
 use Time::HiRes qw(time);
 use File::Path qw(make_path);
 use Cwd qw(abs_path);
-# ИМПОРТИРУЕМ ОБЕ ФУНКЦИИ: для кодирования и декодирования
 use Encode qw(encode_utf8 decode_utf8);
 use Mojo::JSON qw(to_json from_json);
 
@@ -24,13 +25,22 @@ close($lock_fh);
 tie my %pastes, 'DB_File', $db_path, O_RDWR|O_CREAT, 0644
   or die "Cannot open database file $db_path: $!";
 
+# ИСПРАВЛЕНИЕ 1: Загружаем плагин без неверной конфигурации
+plugin 'Prometheus';
+
+# ИСПРАВЛЕНИЕ 2: Получаем объект Net::Prometheus и регистрируем счетчик ОДИН РАЗ.
+# Мы сохраняем объект счетчика в переменную $db_requests_counter.
+my $db_requests_counter = app->prometheus->new_counter(
+    name => 'db_requests_total',
+    help => 'Total requests to the database file.',
+);
+
 # --- Маршруты (API) ---
 
 get '/' => 'index';
 
 post '/' => sub {
     my $c = shift;
-
     my $content = $c->param('content') || '';
     my $lang    = $c->param('language') || 'plaintext';
 
@@ -45,12 +55,12 @@ post '/' => sub {
         created  => time(),
     };
 
+    # ИСПРАВЛЕНИЕ 3: Используем переменную-счетчик для инкремента
+    $db_requests_counter->inc();
+
     open(my $lock, '>>', $lock_file) or die "Cannot open lock file: $!";
     flock($lock, LOCK_EX) or die "Cannot lock database: $!";
-
-    # ИСПРАВЛЕНИЕ: Сначала в JSON, потом кодируем в байты UTF-8 для записи в файл.
     $pastes{$id} = encode_utf8(to_json($data_to_store));
-    
     flock($lock, LOCK_UN);
     close($lock);
 
@@ -61,11 +71,12 @@ get '/paste/:id' => sub {
     my $c = shift;
     my $id = $c->param('id');
 
+    # ИСПРАВЛЕНИЕ 3: Используем ту же переменную-счетчик
+    $db_requests_counter->inc();
+
     open(my $lock, '>>', $lock_file) or die "Cannot open lock file: $!";
     flock($lock, LOCK_SH) or die "Cannot lock database: $!";
-
     my $stored_data_bytes = $pastes{$id};
-    
     flock($lock, LOCK_UN);
     close($lock);
 
@@ -73,9 +84,7 @@ get '/paste/:id' => sub {
         return $c->render(template => 'not_found', status => 404);
     }
 
-    # ИСПРАВЛЕНИЕ: Сначала декодируем байты из файла в строку, потом парсим JSON.
     my $paste = from_json(decode_utf8($stored_data_bytes));
-    
     $c->render('paste', paste => $paste);
 };
 
