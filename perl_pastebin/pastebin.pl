d8c010d2f7a2f43b287b16f270729025721c27ba
haphop228 <krivoshein.2014@list.ru>
2025-07-21 02:00:36 +0300

    fixed bug wtih hypnotoad

--- perl_pastebin/pastebin.pl
+++ perl_pastebin/pastebin.pl
@@ -3,6 +3,8 @@
 use utf8;
 use Mojolicious::Lite;
 use Mojolicious::Plugin::Prometheus;
+# ИСПРАВЛЕНИЕ 1: Явно подключаем плагин Config
+use Mojolicious::Plugin::Config;
 
 use DB_File;
 use Fcntl qw(:flock O_RDWR O_CREAT);
@@ -12,6 +14,17 @@ use File::Path qw(make_path);
 use Cwd qw(abs_path);
 use Encode qw(encode_utf8 decode_utf8);
 use Mojo::JSON qw(to_json from_json);
+
+# ИСПРАВЛЕНИЕ 2: Используем более надежный синтаксис для конфигурации
+app->plugin('Config' => {
+  default => {
+    hypnotoad => {
+      listen  => ['http://*:3000'],
+      workers => 10,
+      pid_file => '/tmp/pastebin.pid'
+    }
+  }
+});
 
 # --- Конфигурация приложения ---
 my $db_dir    = abs_path('data');
@@ -25,7 +38,9 @@ close($lock_fh);
 tie my %pastes, 'DB_File', $db_path, O_RDWR|O_CREAT, 0644
   or die "Cannot open database file $db_path: $!";
 
-plugin 'Prometheus';
+app->plugin('Prometheus' => {
+  duration_buckets => [ 0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1 ],
+});
 
 my $db_requests_counter = app->prometheus->new_counter(
     name => 'db_requests_total',
@@ -33,75 +48,53 @@ my $db_requests_counter = app->prometheus->new_counter(
 );
 
 # --- Маршруты (API) ---
+# (Весь код маршрутов остается без изменений, он был правильным)
 
 get '/' => 'index';
-
-# --- НОВЫЕ ТЕХНИЧЕСКИЕ РУЧКИ ---
-
-# Ручка a) Liveness Probe - проверка, что процесс приложения жив
-get '/healthz' => sub {
-    my $c = shift;
-    $c->render(text => 'OK', status => 200);
-};
-
-# Ручка b) Readiness Probe - проверка, что приложение готово принимать трафик
+get '/healthz' => sub { shift->render(text => 'OK') };
 get '/readyz' => sub {
     my $c = shift;
-    # Проверяем, что мы можем получить доступ к файлу БД (через файл блокировки)
     open(my $lock, '>>', $lock_file) or return $c->render(text => 'DB Unreachable', status => 503);
     flock($lock, LOCK_SH) or return $c->render(text => 'DB Lock Failed', status => 503);
     flock($lock, LOCK_UN);
     close($lock);
-    
-    # Если все проверки прошли, отвечаем что готовы
-    $c->render(text => 'Ready', status => 200);
+    $c->render(text => 'Ready');
 };
-
-# --- Основная логика приложения ---
 
 post '/' => sub {
     my $c = shift;
     my $content = $c->param('content') || '';
     my $lang    = $c->param('language') || 'plaintext';
-
     return $c->render(text => 'Content cannot be empty.', status => 400) if $content eq '';
-
     my $string_to_hash = encode_utf8($content . time . rand());
     my $id = substr(sha1_hex($string_to_hash), 0, 10);
-
-    my $data_to_store = {
-        content  => $content,
-        language => lc($lang),
-        created  => time(),
-    };
-
+    my $data_to_store = { content => $content, language => lc($lang), created => time() };
+    
     $db_requests_counter->inc();
-
+    
     open(my $lock, '>>', $lock_file) or die "Cannot open lock file: $!";
     flock($lock, LOCK_EX) or die "Cannot lock database: $!";
     $pastes{$id} = encode_utf8(to_json($data_to_store));
     flock($lock, LOCK_UN);
     close($lock);
-
+    
     $c->redirect_to("/paste/$id");
 };
 
 get '/paste/:id' => sub {
     my $c = shift;
     my $id = $c->param('id');
-
+    
     $db_requests_counter->inc();
-
+    
     open(my $lock, '>>', $lock_file) or die "Cannot open lock file: $!";
     flock($lock, LOCK_SH) or die "Cannot lock database: $!";
     my $stored_data_bytes = $pastes{$id};
     flock($lock, LOCK_UN);
     close($lock);
-
-    unless (defined $stored_data_bytes) {
-        return $c->render(template => 'not_found', status => 404);
-    }
-
+    
+    return $c->render(template => 'not_found', status => 404) unless defined $stored_data_bytes;
+    
     my $paste = from_json(decode_utf8($stored_data_bytes));
     $c->render('paste', paste => $paste);
 };
